10d23658cbea66da68eef67f6500e70e1b6e348c
masuda yushi <yellow.box0803@gmail.com>
2020-08-14 11:24:06 +0900

    トップページの追加

---
+++ frontend/src/views/Top.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="top">
+    <h2>レシピカテゴリ</h2>
+    <ul class="top_recipe">
+      <li>
+        <a href="#">
+          <img src="" alt="和食">
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <img src="" alt="洋食">
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <img src="" alt="中華">
+        </a>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<style lang="scss">
+  .top_recipe {
+    list-style: none;
+    background: #f7f3a8;
+  }
+  .top_recipe:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+  .top_recipe li {
+    float: left;
+  }
+</style>
+<script>
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Top",
+  // components: {
+  //   HelloWorld
+  // }
+};
+</script>
