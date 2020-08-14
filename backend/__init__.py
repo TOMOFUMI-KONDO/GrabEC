a1db8285774f0ec95e1c8c56e69d5b506356dad9
VIVIDRABBIT0531 <68336123+VIVIDRABBIT0531@users.noreply.github.com>
2020-08-15 01:10:46 +0900

    ログインデータベース作成

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,14 +1,21 @@
+# モジュールインポート
 from flask import Flask, render_template
 from flask_cors import CORS
+# データベースを利用するために追加
 from flask_sqlalchemy import SQLAlchemy
 from backend.api import api
 
-
+# Flaskアプリの生成
 app = Flask('GrabEC',
             static_folder="./dist/static",
             template_folder="./dist")
 app.config.from_object('backend.config.BaseConfig')
+
+# sqlalchemyを通してflaskからdbアクセスをするための入り口
 db = SQLAlchemy(app)
+
+# データベースのimport
+from backend.user import User
 
 app.register_blueprint(api, url_prefix="/api")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
