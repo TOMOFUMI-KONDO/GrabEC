ff5e33991c4c85cafc5bb90cb53e42766b304396
TOMOFUMI-KONDO <ugax2kontomo0314@gmai.com>
2020-08-14 11:14:12 +0900

    ヘッダーコンポーネントの作成

---
+++ frontend/src/components/CartButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-link href="#">
+    <b-button variant="none" class="cart-button">
+      <b-icon icon="cart3" variant="light" font-scale="2.0" class="icon" />
+      <p class="text-light mb-0">カートを見る</p>
+    </b-button>
+  </b-link>
+</template>
+
+<script>
+export default {
+  name: "CartButton"
+};
+</script>
+
+<style lang="scss" scoped>
+.cart-button {
+  &:hover {
+    > * {
+      color: #ff0070 !important;
+    }
+  }
+
+  p {
+    font-size: 14px;
+  }
+}
+</style>
---
+++ frontend/src/components/SearchWindow.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search-window position-relative">
+    <b-input placeholder="キーワードから商品を探す" class="search-input" />
+    <b-icon icon="search" class="search-icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchWindow"
+};
+</script>
+
+<style lang="scss" scoped>
+.search-window {
+  min-width: 400px;
+
+  > * {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .search-input {
+    width: 100%;
+  }
+
+  .search-icon {
+    right: 10px;
+    color: #111;
+  }
+}
+</style>
