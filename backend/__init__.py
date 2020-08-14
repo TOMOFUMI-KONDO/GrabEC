ae13af980ac866a9306def7785c6773ca584cbad
TOMOFUMI-KONDO <ugax2kontomo0314@gmai.com>
2020-08-14 10:06:52 +0900

    データベースの設定

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 from backend.api import api
+
 
 app = Flask('GrabEC',
             static_folder="./dist/static",
             template_folder="./dist")
 app.config.from_object('backend.config.BaseConfig')
+db = SQLAlchemy(app)
 
 app.register_blueprint(api, url_prefix="/api")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,11 @@
 class BaseConfig(object):
-  DEBUG = True
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+        'user': 'mysql',
+        'password': 'mysql',
+        'host': 'localhost',
+        'db_name': 'grabec'
+    })
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
