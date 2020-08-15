a81461beadaab3b6dd0a07706ebe5becde362757
TOMOFUMI-KONDO <ugax2kontomo0314@gmai.com>
2020-08-16 00:20:49 +0900

    Topコンポーネントの作成

--- frontend/src/views/Top.vue
+++ frontend/src/views/Top.vue
@@ -1,48 +1,55 @@
 <template>
-  <div class="top">
-    <h2>レシピカテゴリ</h2>
-    <ul class="top_recipe">
-      <li>
-        <a href="#">
-          <img src="" alt="和食">
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <img src="" alt="洋食">
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <img src="" alt="中華">
-        </a>
-      </li>
-    </ul>
-
+  <div class="top w-75 mx-auto my-5">
+    <section class="menu">
+      <h2 class="mb-4">オススメの献立</h2>
+      <ul class="d-flex justify-content-between">
+        <Menu
+          v-for="menu in menus"
+          :img-name="menu.imgName"
+          :display-name="menu.displayName"
+          :key="menu.imgName"
+        />
+      </ul>
+    </section>
+    <section class="search-window">
+      <h2 class="mb-4">商品を検索</h2>
+      <SearchWindow />
+    </section>
   </div>
 </template>
 
-<style lang="scss">
-  .top_recipe {
-    list-style: none;
-    background: #f7f3a8;
-  }
-  .top_recipe:after {
-    content: '';
-    display: block;
-    clear: both;
-  }
-  .top_recipe li {
-    float: left;
-  }
-</style>
 <script>
-// import HelloWorld from "@/components/HelloWorld.vue";
+import Menu from "@/components/Menu";
+import SearchWindow from "@/components/SearchWindow";
 
 export default {
   name: "Top",
-  // components: {
-  //   HelloWorld
-  // }
+  components: { SearchWindow, Menu },
+  data() {
+    return {
+      menus: [
+        { imgName: "卵焼き.jpg", displayName: "和食" },
+        { imgName: "オムライス.jpg", displayName: "洋食" },
+        { imgName: "餃子.jpg", displayName: "中華" }
+      ]
+    };
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.top {
+  .menu {
+    ul {
+      list-style: none;
+      padding-left: 0;
+
+      li {
+        img {
+          max-width: 300px;
+        }
+      }
+    }
+  }
+}
+</style>
