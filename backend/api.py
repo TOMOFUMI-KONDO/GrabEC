9e3ffcf2fed7afc7cec57d7c8e8335294e38749f
VIVIDRABBIT0531 <68336123+VIVIDRABBIT0531@users.noreply.github.com>
2020-08-15 11:59:22 +0900

    product全取得

--- backend/api.py
+++ backend/api.py
@@ -8,3 +8,13 @@ api = Blueprint('api', __name__)
 def say_hello(name):
   response = {'msg': "Hello {}".format(name)}
   return jsonify(response)
+
+@app.route("/api/product", methods=["GET"])
+# select * from products    
+def getProductList():
+  product_list = db.session.query(Product).all()
+
+  if product_list == None:
+    return[]
+  else:
+    return product_list
--- backend/models.py
+++ backend/models.py
@@ -19,3 +19,36 @@ class Task(db.Model):
 
 def init():
     db.create_all()
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    imgName = db.Column(MEDIUMBLOB)
+    name = db.Column(db.String(1000))
+    cost = db.Column(db.Integer)
+    area = db.Column(db.String(1000))
+    stock = db.Column(db.Integer)
+    review = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Product %r>' % self.name
+
+    def registProduct(product):
+        record = Product(
+            imgName =product['imgName'],
+            name = product['name'],
+            cost = product['cost'],
+            area = product['area'],
+            stock = product['stock'],
+            review = product['review']
+        )
+
+        db.session.add(record)
+        db.session.commit()
+
+        return product
+
+class UserSchema(ma.ModelSchema):
+    class Meta:
+      model = Product
+      fields = ('id','imgName', 'name', 'cost', 'area', 'stock','review')
