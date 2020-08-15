0f61d8add2607ec56438e72e6ec59a98a160d88f
TOMOFUMI-KONDO <ugax2kontomo0314@gmai.com>
2020-08-16 08:43:33 +0900

    axiosをグローバルに読み込むようにした。

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,16 @@ import store from "./store";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.prototype.$api = axios;
 
 new Vue({
   router,
