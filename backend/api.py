fbffb49f39b804daaddde7dacc619c4c0caf04c4
VIVIDRABBIT0531 <68336123+VIVIDRABBIT0531@users.noreply.github.com>
2020-08-16 11:31:04 +0900

    ショッピングカートなんとなくの形

--- backend/api.py
+++ backend/api.py
@@ -1,30 +1,33 @@
 from flask import Blueprint, jsonify
 from random import *
 from backend import db
-from backend.models import Product
+from backend.models import Product, Menu
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello/<string:name>/')
-def say_hello(name):
-  response = {'msg': "Hello {}".format(name)}
-  return jsonify(response)
-
 # Product一覧取得
 @api.route("/product", methods=["GET"])
-# select * from products
 def getProductList():
   product_list = db.session.query(Product).all()
   task_dict = [product.to_dict() for product in product_list]
   return jsonify(task_dict)
 
-  #if product_list == None:
-  #  return[]
-  #else:
-  #  return product_list
+
+@api.route("/menu", methods=["GET"])
+def getMenuList():
+  menu_list = db.session.query(Menu).all()
+  menu_dict = [menu.to_dict() for menu in menu_list]
+  return menu_dict
+
 
 # ショッピングカート
-#@app.route('/api/add_to_cart', methods=['POST'])
-#def add_to_cart():
-  #item = json.loads(request.data.decode('utf-8'))
+# @api.route('/add_to_cart', methods=['POST'])
+# def add_to_cart():
+  # item = json.loads(request.data.decode('utf-8'))
+    #if 'cart_items' not in session:
+        #session['cart_items'] = []
+    #cart_list = session['cart_items']
+
+    #cart_list.append(item)
+    #session['cart_items'] = cart_list
